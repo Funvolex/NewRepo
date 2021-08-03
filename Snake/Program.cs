81c7c8a8bbf59f28e9da4c6edceaa960f93f1b44
Funvolex <reddogdingo@gmail.com>
2021-08-03 23:07:29 +0300

    zaeblO!!!

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,7 +6,7 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1= 5;
+            int x1 = 5;
             int y1 = 5;
             char sym1 = '*';
             Draw(x1, y1, sym1);
@@ -16,9 +16,21 @@ namespace Snake
             char sym2 = '@';
             Draw(x2, y2, sym2);
 
+            /*PointS p1 = new PointS();
+          
+            p1.x = 5;
+            p1.y = 5;
+            p1.sym = '*';
+
+            PointS p2 = new PointS();
+            p2.x = 10; p2.y = 10; p2.sym = '@';
+
+            p1.Draw();
+            p2.Draw();*/
+
             Console.ReadLine();
         }
-        static void Draw(int x,int y,char sym)
+        static void Draw(int x, int y, char sym)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
